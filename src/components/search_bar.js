a88062678f7143739e558497dbba5d2b2c7a10f9
Helder Ferreira <helder.ferreira@pingpost.pt>
2018-06-21 17:50:35 +0100

    -> Merged ReduxSimpleStarter contents directory to the main folder

---
+++ src/components/search_bar.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+// Teste
+class SearchBar extends Component {
+    render() {
+        return <input/>;
+    }
+}
+
+export default SearchBar;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDJSosuBDU0rOX__G8oAv3qOCUrYg2CYaY';
+
+// Create a new component. This component should produce
+// Some HTML
+const App = () => {
+    // JSX is an javascript extension/dialect that let us write html in js file and transform it on pure javascript syntax, we can see this transformation using for example the babel sandbox on their website
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
+};
+
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
