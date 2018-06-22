38a857c0b3b82c553580a3242eba0198fbc55900
Helder Ferreira <helder.ferreira@pingpost.pt>
2018-06-22 11:58:50 +0100

    -> End Section 1 course

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import SearchBar from './components/search_bar';
+import Clock from './components/Clock';
 
 const API_KEY = 'AIzaSyDJSosuBDU0rOX__G8oAv3qOCUrYg2CYaY';
 
@@ -12,10 +13,11 @@ const App = () => {
     return (
         <div>
             <SearchBar/>
+            {/*<Clock/>*/}
         </div>
     );
 };
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(<App/>, document.querySelector('.container'));
